90d4a1dfe16771864242b1494f8b7c316caec6af
sanat <sanatjha4@gmail.com>
2020-12-21 12:00:47 +0530

    Main

---
+++ home page/home.js
@@ -0,0 +1,8 @@
+function hamburger() {
+    var x = document.getElementById("topband-hamburger");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
