259397f7b2c934cb492b7ea772143c22fe07ec8d
sanat <sanatjha4@gmail.com>
2020-12-24 10:34:45 +0530

    Main

--- home page/home.js
+++ home page/home.js
@@ -1,8 +1,16 @@
 function hamburger() {
-    var x = document.getElementById("topband-hamburger");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+  var x = document.getElementById("topband-hamburger");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
+}
+function search() {
+  var x = document.getElementById("search");
+  if (x.style.display === "inline") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "inline";
+  }
+}
