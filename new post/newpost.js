04f4f3fea4e5fa4ac271a98b7df09baad1716c49
sanat <sanatjha4@gmail.com>
2020-12-31 18:02:38 +0530

    Main

---
+++ new post/newpost.js
@@ -0,0 +1,27 @@
+function readURL(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+        $("#blah").attr("src", e.target.result);
+      };
+
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+  var modal = document.getElementById("Hastag-popup-back");
+  var btn = document.getElementById("HastagButton");
+  var span = document.getElementsByClassName(
+    "Hastag-popUp-close-Button"
+  )[0];
+  btn.onclick = function () {
+    modal.style.display = "block";
+  };
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
